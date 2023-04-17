6ec2dfce699f3df9dd55b253ab6b302417eeda01
guillaume <guillaume.belanger27@gmail.com>
2023-04-17 18:09:03 -0400

    Bumbs github actions versions

--- src/charm.py
+++ src/charm.py
@@ -130,7 +130,7 @@ class NRFOperatorCharm(CharmBase):
                     "nrf": {
                         "override": "replace",
                         "startup": "enabled",
-                        "command": f"/free5gc/nrf/nrf --nrfcfg {BASE_CONFIG_PATH}/{CONFIG_FILE_NAME}",
+                        "command": f"/free5gc/nrf/nrf --nrfcfg {BASE_CONFIG_PATH}/{CONFIG_FILE_NAME}",  # noqa: E501
                         "environment": self._environment_variables,
                     },
                 },
