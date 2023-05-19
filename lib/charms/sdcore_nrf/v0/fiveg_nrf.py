dfe7b0ea7fceb460f16975a56eb377c74a539fc3
Javier Sanchez <sanchezfdez.javier@gmail.com>
2023-05-19 10:24:43 +0000

    Adds pytest-interaface-tester back to required deps

--- lib/charms/sdcore_nrf/v0/fiveg_nrf.py
+++ lib/charms/sdcore_nrf/v0/fiveg_nrf.py
@@ -41,7 +41,7 @@ class DummyFiveGNRFRequirerCharm(CharmBase):
         self.framework.observe(self.nrf_requirer.on.nrf_available, self._on_nrf_available)
 
     def _on_nrf_available(self, event: NRFAvailableEvent):
-        nrf_url = event.url
+        nrf_url = self.nrf_requirer.nrf_url
         <do something with the nrf_url>
 
 
@@ -110,10 +110,10 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 2
+LIBPATCH = 3
 
 PYDEPS = [
-    "pydantic",
+    "pydantic", "pytest-interface-tester"
 ]
 
 
