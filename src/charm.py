a9b6876ae1b90044503078e921dda4a78d846085
yazansalti <yazan.salti@canonical.com>
2023-05-23 16:46:04 +0400

    Improves the charm code Adds new unit test cases

--- src/charm.py
+++ src/charm.py
@@ -27,6 +27,7 @@ BASE_CONFIG_PATH = "/etc/nrf"
 CONFIG_FILE_NAME = "nrfcfg.yaml"
 DATABASE_NAME = "free5gc"
 NRF_SBI_PORT = 29510
+DATABASE_RELATION_NAME = "database"
 
 
 class NRFOperatorCharm(CharmBase):
@@ -38,11 +39,11 @@ class NRFOperatorCharm(CharmBase):
         self._container_name = self._service_name = "nrf"
         self._container = self.unit.get_container(self._container_name)
         self._database = DatabaseRequires(
-            self, relation_name="database", database_name=DATABASE_NAME
+            self, relation_name=DATABASE_RELATION_NAME, database_name=DATABASE_NAME
         )
-        self.framework.observe(self.on.database_relation_joined, self._configure_pebble_layer)
-        self.framework.observe(self.on.nrf_pebble_ready, self._configure_pebble_layer)
-        self.framework.observe(self._database.on.database_created, self._on_database_created)
+        self.framework.observe(self.on.database_relation_joined, self._configure_nrf)
+        self.framework.observe(self.on.nrf_pebble_ready, self._configure_nrf)
+        self.framework.observe(self._database.on.database_created, self._configure_nrf)
         self._service_patcher = KubernetesServicePatch(
             charm=self,
             ports=[
@@ -50,38 +51,7 @@ class NRFOperatorCharm(CharmBase):
             ],
         )
 
-    def _on_database_created(self, event: DatabaseCreatedEvent) -> None:
-        """Handle database created event.
-
-        Args:
-            event: DatabaseCreatedEvent
-        """
-        if not self._container.can_connect():
-            self.unit.status = WaitingStatus("Waiting for container to be ready")
-            event.defer()
-            return
-        self._write_config_file(
-            database_url=event.uris.split(",")[0],  # type: ignore[union-attr]
-        )
-        self._configure_pebble_layer(event)
-
-    def _write_config_file(self, database_url: str) -> None:
-        """Writes config file to workload.
-
-        Args:
-            database_url: Database URL
-        """
-        jinja2_environment = Environment(loader=FileSystemLoader("src/templates/"))
-        template = jinja2_environment.get_template("nrfcfg.yaml.j2")
-        content = template.render(
-            database_name=DATABASE_NAME,
-            database_url=database_url,
-            nrf_sbi_port=NRF_SBI_PORT,
-        )
-        self._container.push(path=f"{BASE_CONFIG_PATH}/{CONFIG_FILE_NAME}", source=content)
-        logger.info(f"Pushed {CONFIG_FILE_NAME} config file")
-
-    def _configure_pebble_layer(
+    def _configure_nrf(
         self, event: Union[PebbleReadyEvent, RelationJoinedEvent, DatabaseCreatedEvent]
     ) -> None:
         """Adds pebble layer and manages Juju unit status.
@@ -89,29 +59,48 @@ class NRFOperatorCharm(CharmBase):
         Args:
             event: Juju event
         """
-        if not self._database_relation_is_created:
-            self.unit.status = BlockedStatus("Waiting for database relation to be created")
-            return
         if not self._container.can_connect():
             self.unit.status = WaitingStatus("Waiting for container to be ready")
+            event.defer()
+            return
+        if not self._relation_created(DATABASE_RELATION_NAME):
+            self.unit.status = BlockedStatus("Waiting for database relation to be created")
+            event.defer()
+            return
+        if not self._database_info:
+            self.unit.status = WaitingStatus("Waiting for database info to be available")
             event.defer()
             return
         if not self._container.exists(path=BASE_CONFIG_PATH):
             self.unit.status = WaitingStatus("Waiting for storage to be attached")
             event.defer()
             return
-        self._container.add_layer("nrf", self._pebble_layer, combine=True)
-        self._container.replan()
+        content = self._render_config(database_url=self._database_info["uris"].split(",")[0])
+        self._push_config_file(content=content)
+        if not self._config_file_is_written():
+            self.unit.status = WaitingStatus("Waiting for config file to be written")
+            event.defer()
+            return
+        self._configure_workload()
         self.unit.status = ActiveStatus()
 
-    @property
-    def _database_relation_is_created(self) -> bool:
-        """Returns whether database relation is created.
+    def _configure_workload(self):
+        """Configures pebble layer for the amf container."""
+        plan = self._container.get_plan()
+        layer = self._pebble_layer
+        if plan.services != layer.services:
+            self._container.add_layer("nrf", layer, combine=True)
+            self._container.restart(self._service_name)
+
+    def _config_file_is_written(self) -> bool:
+        """Returns whether the config file was written to the workload container.
 
         Returns:
-            bool: Whether database relation is created.
+            bool: Whether the config file was written.
         """
-        return self._relation_created("database")
+        if not self._container.exists(f"{BASE_CONFIG_PATH}/{CONFIG_FILE_NAME}"):
+            return False
+        return True
 
     def _relation_created(self, relation_name: str) -> bool:
         """Returns whether a given Juju relation was crated.
@@ -123,6 +112,48 @@ class NRFOperatorCharm(CharmBase):
             bool: Whether the relation was created.
         """
         return bool(self.model.get_relation(relation_name))
+
+    @staticmethod
+    def _render_config(database_url: str) -> str:
+        jinja2_environment = Environment(loader=FileSystemLoader("src/templates/"))
+        template = jinja2_environment.get_template("nrfcfg.yaml.j2")
+        content = template.render(
+            database_name=DATABASE_NAME,
+            database_url=database_url,
+            nrf_sbi_port=NRF_SBI_PORT,
+        )
+        return content
+
+    def _push_config_file(self, content: str) -> None:
+        """Pushes config file to workload.
+
+        Args:
+            content: config file content
+        """
+        if not self._container.can_connect():
+            return
+        self._container.push(path=f"{BASE_CONFIG_PATH}/{CONFIG_FILE_NAME}", source=content)
+        logger.info(f"Pushed {CONFIG_FILE_NAME} config file")
+
+    @property
+    def _database_is_available(self) -> bool:
+        """Returns True if the database is available.
+
+        Returns:
+            bool: True if the database is available.
+        """
+        return self._database.is_resource_created()
+
+    @property
+    def _database_info(self) -> dict:
+        """Returns the database data.
+
+        Returns:
+            Dict: The database data.
+        """
+        if not self._database_is_available:
+            return {}
+        return self._database.fetch_relation_data()[self._database.relations[0].id]
 
     @property
     def _pebble_layer(self) -> Layer:
--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -9,6 +9,10 @@ from ops.model import ActiveStatus, BlockedStatus, WaitingStatus
 
 from charm import NRFOperatorCharm  # type: ignore[import]
 
+DB_APPLICATION_NAME = "mongodb-k8s"
+BASE_CONFIG_PATH = "/etc/nrf"
+CONFIG_FILE_NAME = "nrfcfg.yaml"
+
 
 class TestCharm(unittest.TestCase):
     @patch(
@@ -20,53 +24,44 @@ class TestCharm(unittest.TestCase):
         self.addCleanup(self.harness.cleanup)
         self.harness.begin()
 
-    def create_database_relation(self) -> int:
-        relation_id = self.harness.add_relation(relation_name="database", remote_app="mongodb-k8s")
-        self.harness.add_relation_unit(relation_id=relation_id, remote_unit_name="mongodb-k8s/0")
+    def _create_database_relation(self) -> int:
+        """Create a database relation.
+
+        Returns:
+            relation_id: ID of the created relation
+        """
+        relation_id = self.harness.add_relation(
+            relation_name="database",
+            remote_app=DB_APPLICATION_NAME,
+        )
+        self.harness.add_relation_unit(
+            relation_id=relation_id,
+            remote_unit_name=f"{DB_APPLICATION_NAME}/0",
+        )
         return relation_id
 
-    @patch("ops.model.Container.push")
-    def test_given_can_connect_to_workload_container_when_database_created_event_then_config_file_is_written(  # noqa: E501
-        self,
-        patch_push,
-    ):
-        uri_0 = "1.2.3.4:1234"
-        uri_1 = "5.6.7.8:1111"
-        self.harness.set_can_connect(container="nrf", val=True)
-        relation_data = {
-            "username": "banana",
-            "password": "password123",
-            "uris": "".join([uri_0, ",", uri_1]),
-        }
-        relation_id = self.create_database_relation()
-
+    def _database_is_available(self) -> None:
+        """Create a database relation and set the database information."""
+        database_relation_id = self._create_database_relation()
         self.harness.update_relation_data(
-            relation_id=relation_id, app_or_unit="mongodb-k8s", key_values=relation_data
+            relation_id=database_relation_id,
+            app_or_unit=DB_APPLICATION_NAME,
+            key_values={
+                "username": "dummy",
+                "password": "dummy",
+                "uris": "http://dummy",
+            },
         )
 
-        patch_push.assert_called_with(
-            path="/etc/nrf/nrfcfg.yaml",
-            source=f'configuration:\n  DefaultPlmnId:\n    mcc: "208"\n    mnc: "93"\n  MongoDBName: free5gc\n  MongoDBUrl: { uri_0 }\n  mongoDBStreamEnable: true\n  mongodb:\n    name: free5gc\n    url: { uri_0 }\n  nfKeepAliveTime: 60\n  nfProfileExpiryEnable: true\n  sbi:\n    bindingIPv4: 0.0.0.0\n    port: 29510\n    registerIPv4: nrf\n    scheme: http\n  serviceNameList:\n  - nnrf-nfm\n  - nnrf-disc\ninfo:\n  description: NRF initial local configuration\n  version: 1.0.0\nlogger:\n  AMF:\n    ReportCaller: false\n    debugLevel: info\n  AUSF:\n    ReportCaller: false\n    debugLevel: info\n  Aper:\n    ReportCaller: false\n    debugLevel: info\n  CommonConsumerTest:\n    ReportCaller: false\n    debugLevel: info\n  FSM:\n    ReportCaller: false\n    debugLevel: info\n  MongoDBLibrary:\n    ReportCaller: false\n    debugLevel: info\n  N3IWF:\n    ReportCaller: false\n    debugLevel: info\n  NAS:\n    ReportCaller: false\n    debugLevel: info\n  NGAP:\n    ReportCaller: false\n    debugLevel: info\n  NRF:\n    ReportCaller: false\n    debugLevel: info\n  NamfComm:\n    ReportCaller: false\n    debugLevel: info\n  NamfEventExposure:\n    ReportCaller: false\n    debugLevel: info\n  NsmfPDUSession:\n    ReportCaller: false\n    debugLevel: info\n  NudrDataRepository:\n    ReportCaller: false\n    debugLevel: info\n  OpenApi:\n    ReportCaller: false\n    debugLevel: info\n  PCF:\n    ReportCaller: false\n    debugLevel: info\n  PFCP:\n    ReportCaller: false\n    debugLevel: info\n  PathUtil:\n    ReportCaller: false\n    debugLevel: info\n  SMF:\n    ReportCaller: false\n    debugLevel: info\n  UDM:\n    ReportCaller: false\n    debugLevel: info\n  UDR:\n    ReportCaller: false\n    debugLevel: info\n  WEBUI:\n    ReportCaller: false\n    debugLevel: info',  # noqa: E501
-        )
-
-    @patch("ops.model.Container.push")
-    def test_given_cant_connect_to_workload_container_when_database_created_event_then_config_file_is_not_pushed(  # noqa: E501
+    def test_given_container_not_ready_when_database_relation_joins_then_status_is_waiting(
         self,
-        patch_push,
     ):
         self.harness.set_can_connect(container="nrf", val=False)
-        relation_data = {
-            "username": "banana",
-            "password": "password123",
-            "uris": "1.2.3.4:1234,5.6.7.8:1111",
-        }
-        relation_id = self.create_database_relation()
+        self._create_database_relation()
 
-        self.harness.update_relation_data(
-            relation_id=relation_id, app_or_unit="mongodb-k8s", key_values=relation_data
+        self.assertEqual(
+            self.harness.model.unit.status, WaitingStatus("Waiting for container to be ready")
         )
-
-        patch_push.not_called()
 
     def test_given_database_relation_not_created_when_pebble_ready_then_status_is_blocked(self):
         self.harness.container_pebble_ready(container_name="nrf")
@@ -76,13 +71,69 @@ class TestCharm(unittest.TestCase):
             BlockedStatus("Waiting for database relation to be created"),
         )
 
+    def test_given_database_information_not_available_when_pebble_ready_then_status_is_waiting(
+        self,
+    ):
+        self._create_database_relation()
+        self.harness.container_pebble_ready(container_name="nrf")
+        self.assertEqual(
+            self.harness.model.unit.status,
+            WaitingStatus("Waiting for database info to be available"),
+        )
+
+    def test_given_storage_not_attached_when_pebble_ready_then_status_is_waiting(
+        self,
+    ):
+        self._database_is_available()
+        self.harness.container_pebble_ready(container_name="nrf")
+
+        self.assertEqual(
+            self.harness.model.unit.status,
+            WaitingStatus("Waiting for storage to be attached"),
+        )
+
     @patch("ops.model.Container.exists")
-    def test_given_config_file_is_written_when_pebble_ready_then_pebble_plan_is_applied(
-        self, patch_exists
+    @patch("ops.model.Container.push")
+    def test_given_database_info_and_storage_attached_when_pebble_ready_then_config_file_is_rendered_and_pushed(  # noqa: E501
+        self,
+        patch_push,
+        patch_exists,
+    ):
+        patch_exists.return_value = True
+        self._database_is_available()
+        self.harness.container_pebble_ready(container_name="nrf")
+        with open("tests/unit/expected_config/config.conf") as expected_config_file:
+            expected_content = expected_config_file.read()
+            patch_push.assert_called_with(
+                path=f"{BASE_CONFIG_PATH}/{CONFIG_FILE_NAME}",
+                source=expected_content.strip(),
+            )
+
+    @patch("ops.model.Container.exists")
+    @patch("ops.model.Container.push")
+    def test_given_config_file_push_unsuccessful_when_pebble_ready_then_status_is_waiting(
+        self,
+        patch_push,
+        patch_exists,
+    ):
+        patch_exists.side_effect = [True, False]
+        self._database_is_available()
+        self.harness.container_pebble_ready(container_name="nrf")
+        self.assertEqual(
+            self.harness.model.unit.status,
+            WaitingStatus("Waiting for config file to be written"),
+        )
+
+    @patch("ops.model.Container.exists")
+    @patch("ops.model.Container.push")
+    def test_given_config_pushed_when_pebble_ready_then_pebble_plan_is_applied(
+        self,
+        patch_push,
+        patch_exists,
     ):
         patch_exists.return_value = True
 
-        self.create_database_relation()
+        self._database_is_available()
 
         self.harness.container_pebble_ready(container_name="nrf")
 
@@ -108,38 +159,17 @@ class TestCharm(unittest.TestCase):
         self.assertEqual(expected_plan, updated_plan)
 
     @patch("ops.model.Container.exists")
-    def test_given_config_file_is_not_written_when_pebble_ready_then_status_is_waiting(
-        self, patch_exists
-    ):
-        patch_exists.return_value = False
-        self.create_database_relation()
-
-        self.harness.container_pebble_ready(container_name="nrf")
-
-        self.assertEqual(
-            self.harness.model.unit.status, WaitingStatus("Waiting for config file to be written")
-        )
-
-    @patch("ops.model.Container.exists")
+    @patch("ops.model.Container.push")
     def test_given_database_relation_is_created_and_config_file_is_written_when_pebble_ready_then_status_is_active(  # noqa: E501
-        self, patch_exists
+        self,
+        patch_push,
+        patch_exists,
     ):
         patch_exists.return_value = True
-        self.create_database_relation()
 
+        self._database_is_available()
+
+        self.harness.container_pebble_ready(container_name="nrf")
         self.harness.container_pebble_ready("nrf")
 
         self.assertEqual(self.harness.model.unit.status, ActiveStatus())
-
-    @patch("ops.model.Container.exists")
-    def test_given_container_not_ready_when_database_relation_joins_then_status_is_waiting(
-        self, patch_exists
-    ):
-        patch_exists.return_value = True
-        self.create_database_relation()
-
-        self.harness.set_can_connect(container="nrf", val=False)
-
-        self.assertEqual(
-            self.harness.model.unit.status, WaitingStatus("Waiting for container to be ready")
-        )
